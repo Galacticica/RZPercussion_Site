69eca27e158a39808931c7a8e04738b04cee1f94
Reagan Zierke <reaganzierke@gmail.com>
2024-10-27 13:52:56 -0500

    new view

--- performances/views.py
+++ performances/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Performed_Piece, Instruments
 
 
 def index(request):
     latest_performances = Performed_Piece.objects.order_by("-date")[:5]
-    output = ", ".join([p.title for p in latest_performances])
-    return HttpResponse(output) 
+    
+    template = loader.get_template("performances/index.html")
+    context = {"latest_performances" : latest_performances}
+    return HttpResponse(template.render(context, request)) 
 
 def performance(request, performed_piece_id):
     return HttpResponse("You're looking at piece %s" % performed_piece_id)

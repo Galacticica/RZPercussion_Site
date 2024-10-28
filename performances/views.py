921a297cab05c3080c421ce0be9155c34003a61b
Reagan Zierke <reaganzierke@gmail.com>
2024-10-28 13:25:22 -0500

    the real if everything breaks commit lol

--- performances/views.py
+++ performances/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Performed_Piece
+from .models import Performed_Piece 
 
 
 def index(request):

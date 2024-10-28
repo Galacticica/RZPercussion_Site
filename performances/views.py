389a4edf5c139fe4f44ca29af2b30de7788d9cd5
Reagan Zierke <reaganzierke@gmail.com>
2024-10-28 13:25:00 -0500

    just in case everything breaks

--- performances/views.py
+++ performances/views.py
@@ -3,12 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Performed_Piece, Instruments
+from .models import Performed_Piece
 
 
 def index(request):
-    latest_performances = Performed_Piece.objects.order_by("-date")[:5]
-    
+    latest_performances = Performed_Piece.objects.order_by("-date")
     template = loader.get_template("performances/index.html")
     context = {"latest_performances" : latest_performances}
     return HttpResponse(template.render(context, request)) 
@@ -16,7 +15,6 @@ def index(request):
 def performance(request, performed_piece_id):
     piece = get_object_or_404(Performed_Piece, pk=performed_piece_id)
     insts = [instrument.name for instrument in piece.instruments.all()]
-    print(insts)
     template = loader.get_template("performances/performance.html")
     context = {"piece" : piece, "instruments" : insts}
     return HttpResponse(template.render(context, request))

8f2b862132b456dca5181977cc7c0a0317263f40
Reagan Zierke <reaganzierke@gmail.com>
2024-11-01 09:25:17 -0500

    fixed user auth !

--- home/views.py
+++ home/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 def main(request):
+  print(f"User authenticated: {request.user.is_authenticated}")
+  print(f"User: {request.user}")
+  print(f"Session data: {request.session.items()}")
+
   template = loader.get_template('home/main.html')
-  return HttpResponse(template.render())
+  context={}
+  return HttpResponse(template.render(context, request))

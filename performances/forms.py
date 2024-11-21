f69e3642189927c7eb679f1309b3b5053d9c8f2d
Reagan Zierke <reaganzierke@gmail.com>
2024-11-20 22:00:38 -0600

    styling

--- performances/forms.py
+++ performances/forms.py
@@ -2,9 +2,24 @@ from django import forms
 from .models import Performed_Piece, Instruments
 
 class PerformanceSearchForm(forms.Form):
-    title_query = forms.CharField(max_length=255, required=False, label="Title")
-    composer_query = forms.CharField(max_length=255, required=False, label="Composer / Arranger")
-    instrument_query = forms.ModelMultipleChoiceField(queryset=Instruments.objects.all(), widget=forms.CheckboxSelectMultiple, label="Instruments Used", required=False)
+    title_query = forms.CharField(
+        max_length=255,
+        required=False,
+        label="Title",
+        widget=forms.TextInput(attrs={'class': 'styled-input'})  
+    )
+    composer_query = forms.CharField(
+        max_length=255,
+        required=False,
+        label="Composer / Arranger",
+        widget=forms.TextInput(attrs={'class': 'styled-input'})  
+    )
+    instrument_query = forms.ModelMultipleChoiceField(
+        queryset=Instruments.objects.all(),
+        widget=forms.SelectMultiple(attrs={'class': 'multi-checkbox-dropdown'}),  
+        label="Instruments Used",
+        required=False
+    )
 
     class Meta:
         model = Performed_Piece

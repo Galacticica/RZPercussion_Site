e8a106f7071c054897576f31f41f159d6af934f2
Reagan Zierke <reaganzierke@gmail.com>
2024-11-23 14:46:30 -0600

    created composition model

--- compositions/admin.py
+++ compositions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Composition
 
-# Register your models here.
+class CompositionAdmin(admin.ModelAdmin):
+    list_display = ("title", "piece_type")
+    prepopulated_fields = {"slug" : ("title", "piece_type")}
+
+admin.site.register(Composition ,CompositionAdmin)

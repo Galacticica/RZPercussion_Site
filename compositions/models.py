4540dafd65d6c3bf004e0f13fb7c9de3bb54af69
Reagan Zierke <reaganzierke@gmail.com>
2024-11-23 14:23:04 -0600

    created composition model

--- compositions/models.py
+++ compositions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from performances.models import Instruments
 
-# Create your models here.
+class Composition(models.Model):
+    title = models.CharField(max_length=100)
+    og_composer = models.CharField(max_length=200, blank=True)
+    description = models.TextField()
+    link = models.URLField(max_length=200)
+    piece_type = models.CharField(max_length=100)
+    instruments = models.ManyToManyField(Instruments, related_name='composed_pieces')
+    complete = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
+    
+    def __str__(self):
+        return self.title
